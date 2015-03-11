5238b9a44405559764ef023917acf261681c73fd
SakuraiSatoru <fangzhihao126@gmail.com>
2015-03-11 12:06:25 +0900

    update google.js
    format filenames

--- UpdateJS.py
+++ UpdateJS.py
@@ -10,20 +10,43 @@ def cur_file_dir():
      elif os.path.isfile(path):
          return os.path.dirname(path)
 
+             
+
+
 
 rootdir = cur_file_dir()
 imgdir = cur_file_dir()+"\images"
-arr = []
+arr0 = []
+#arr = []
+
+i=1
+while True:
+     if os.path.isfile(imgdir+"\\"+str(i)+".jpg"):
+          arr0.append(str(i)+".jpg")
+          i = i+1
+     elif os.path.isfile(imgdir+"\\"+str(i)+".png"):
+          arr0.append(str(i)+".png")
+          i = i+1
+     elif os.path.isfile(imgdir+"\\"+str(i)+".bmp"):
+          arr0.append(str(i)+".bmp")
+          i = i+1
+     else:
+          break
+
+
+
+
 
 for parent,dirnames,filenames in os.walk(imgdir):
     for filename in filenames:
         if filename != "Transparent_google_logo.png":
-             if filename[-3:] == "jpg" or filename[-3:] == "png":
-                  print filename
-                  arr.append(filename)
+             if (filename[-3:] == "jpg" or filename[-3:] == "png" or filename[-3:] == "bmp") and (not filename in arr0):
+                  os.rename(imgdir+"\\"+filename,imgdir+"\\"+str(i)+filename[-4:])
+                  print str(i)+filename[-4:]
+                  arr0.append(str(i)+filename[-4:])
+                  i += 1
 
-for a in arr:
-    print a
+
 
 fileObject = open('content\google.js')
 fileContent = fileObject.read()
@@ -33,7 +56,8 @@ pos1 = fileContent.find("images:") +8
 pos2 = pos1 + fileContent[pos1:].find("],")
 
 str = ""
-for a in arr:
+for a in arr0:
+    print a
     a = "chrome.extension.getURL('images/"+a+"')"
     str += a+","
 str = str[:-1]
